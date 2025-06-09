bfe8cc447badc919951e3c5c38a35158aae004c9
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-06-09 17:38:45 +0500

    feat: Introduce internal packages for analysis and environment

---
+++ lib/bootstrap.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+Future<void> bootstrap({
+  required FutureOr<Widget> Function() applicationBuilder,
+}) async {
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      Bloc.observer = ApplicationBlocObserver();
+      runApp(await applicationBuilder());
+    },
+    (error, stack) {
+      log('Error: $error', stackTrace: stack);
+    },
+  );
+}
+
+class ApplicationBlocObserver extends BlocObserver {
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
---
+++ lib/source/authorization/pages/authorization_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yandex_eats_clone_application/source/source.dart';
+
+class AuthorizationPage extends StatelessWidget {
+  const AuthorizationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AuthorizationView();
+  }
+}
