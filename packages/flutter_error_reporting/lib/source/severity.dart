aea4ae55104bea8b6355afe91b9448b93e31b012
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-06-10 17:26:16 +0500

    docs: Document Severity enum

--- packages/flutter_error_reporting/lib/source/severity.dart
+++ packages/flutter_error_reporting/lib/source/severity.dart
@@ -1,1 +1,19 @@
-enum Severity { debug, info, warning, error, fatal }
+/// {@template severity}
+/// Defines the severity level of an event or message.
+/// {@endtemplate}
+enum Severity {
+  /// Debugging information.
+  debug,
+
+  /// Informational messages.
+  info,
+
+  /// Warning messages.
+  warning,
+
+  /// Error messages.
+  error,
+
+  /// Fatal error messages, indicating a critical failure.
+  fatal,
+}
