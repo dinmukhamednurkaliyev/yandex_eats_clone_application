2125815783eb65a39751e788634c043e0d2c6e2c
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-06-11 17:23:56 +0500

    refactor(app): Adopt flutter_bootstrapper for application initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bootstrapper/flutter_bootstrapper.dart';
 import 'package:flutter_error_reporting/source/source.dart';
 import 'package:flutter_logger/flutter_logger.dart';
-import 'package:yandex_eats_clone_application/bootstrap.dart';
+import 'package:yandex_eats_clone_application/application_bloc_observer.dart';
 import 'package:yandex_eats_clone_application/source/source.dart';
 
 Future<void> main() async {
   final ErrorReporterService errorReporterService = DebugErrorReporter();
+  final blocObserver = ApplicationBlocObserver(
+    errorReporterService: errorReporterService,
+  );
+  final applicationBootstrapper = ApplicationBootstrapper(
+    errorReporterService: errorReporterService,
+    blocObserver: blocObserver,
+  );
   final Initializer initializer = () async {
     await errorReporterService.initialize(
       ErrorReporterOptions(enableInDebugMode: true),
     );
     await Firebase.initializeApp();
-    Logger.instance.info('Application initializations complete.');
+    Logger.instance.info(
+      'Application-specific initializations complete.',
+      name: 'Application.Main',
+    );
   };
   final applicationBuilder = () {
     return const AuthorizationPage();
   };
 
-  await bootstrap(
+  await applicationBootstrapper.run(
     applicationBuilder: applicationBuilder,
     onInitialize: initializer,
-    errorReporterService: errorReporterService,
   );
 }
